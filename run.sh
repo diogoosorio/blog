e86e22593b70ab2257f5b0ae59b64b5b5c2234c7
Diogo Osório <diogo.g.osorio@gmail.com>
2013-11-23 21:11:57 +0000

    Made the run script a tad more flexible.

--- run.sh
+++ run.sh
@@ -1,12 +1,27 @@
 #!/bin/bash
 
+UWSGI_USER="uwsgi"
+UWSGI_GROUP="uwsgi"
+PID_FILE=uwsgi.pid
+MAX_REQUESTS=50
+WORKERS=1
+SOCKET="127.0.0.1:8889"
+
+# Check if the virtual environment is present
 if [ ! -d "venv" ]; then
     virtualenv venv
     venv/bin/pip install -r requirements.txt
 fi
 
+# Check if uwsgi is present
 if [ ! -f "venv/bin/uwsgi" ]; then
     venv/bin/pip install uwsgi
 fi
 
-venv/bin/uwsgi -s /tmp/diogoosorio.com.sock --module blog_app.blog_app --callable app
+# Find the uwsgi run user
+UWSGI_UID=$(id -u $UWSGI_USER)
+UWSGI_GID=$(id -g $UWSGI_GROUP)
+
+venv/bin/uwsgi -s $SOCKET --master --module blog_app.blog_app --callable app \
+    --workers $WORKERS --harakiri 30 --max-requests $MAX_REQUESTS --pidfile $PID_FILE \
+    --uid $UWSGI_UID --gid $UWSGI_GID
