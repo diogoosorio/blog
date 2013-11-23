e128cc423582e87fc69d8a7f65dcad034391bd80
Diogo Osório <diogo.g.osorio@gmail.com>
2013-11-23 21:18:10 +0000

    Some  more tweaks to the run script.

--- run.sh
+++ run.sh
@@ -6,6 +6,7 @@ PID_FILE=uwsgi.pid
 MAX_REQUESTS=50
 WORKERS=1
 SOCKET="127.0.0.1:8889"
+LOG_DIR="./logs"
 
 # Check if the virtual environment is present
 if [ ! -d "venv" ]; then
@@ -18,10 +19,17 @@ if [ ! -f "venv/bin/uwsgi" ]; then
     venv/bin/pip install uwsgi
 fi
 
+# Add the log directory
+if [ ! -d $LOG_DIR ]; then
+    mkdir $LOG_DIR
+    chown -R $UWSGI_USER $LOG_DIR
+    chgrp -R $UWSGI_GROUP $LOG_DIR
+fi
+
 # Find the uwsgi run user
 UWSGI_UID=$(id -u $UWSGI_USER)
 UWSGI_GID=$(id -g $UWSGI_GROUP)
 
 venv/bin/uwsgi -s $SOCKET --master --module blog_app.blog_app --callable app \
     --workers $WORKERS --harakiri 30 --max-requests $MAX_REQUESTS --pidfile $PID_FILE \
-    --uid $UWSGI_UID --gid $UWSGI_GID
+    --uid $UWSGI_UID --gid $UWSGI_GID --logto $LOG_DIR
